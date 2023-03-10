723296052a9aa2614f86a8c4671c0317bac20b6c
Federica Rizza <uni303002@studium.unict.it>
2023-03-10 21:50:06 +0100

    Comunication with client added

--- server/server.go
+++ server/server.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 
-	"github.com/Henry-Sarabia/igdb"
+	"github.com/Henry-Sarabia/igdb/v2"
 )
 
 const (
@@ -54,68 +56,93 @@ func handleClient(conn net.Conn) {
 		switch request { 
 		case "REGISTER ":
 			usr,_:=bufio.NewReader(conn).ReadString(' ')
+			usr = strings.ReplaceAll(usr, " ", "") //per togliere lo spazio dopo il nome
 			psw,_:=bufio.NewReader(conn).ReadString('\n')
+			psw = strings.ReplaceAll(psw, "\n", "")
 			done:= register(usr,psw) 
 			if done {
 				buffer=[]byte("1")
 				} else {
 					buffer=[]byte("0")
 				}
-				conn.Write(buffer) //gestire err?
+			conn.Write(buffer) //gestire err?
+
 		case "LOGIN ":
 			usr,_:=bufio.NewReader(conn).ReadString(' ')
+			usr = strings.ReplaceAll(usr, " ", "")
 			psw,_:=bufio.NewReader(conn).ReadString('\n')
-			done=login(usr,psw,&user) 
+			psw = strings.ReplaceAll(psw, "\n", "")
+
+			done:=login(usr,psw,&utente)
 			if done {
 				buffer=[]byte("1")
+				conn.Write(buffer) //devo mandare i dati utente anche al client? SI
+				jsonUtente,_ := json.Marshal(utente)
+				conn.Write(jsonUtente)
 				} else {
 					buffer=[]byte("0")
+					conn.Write(buffer)
 				}
-				conn.Write(buffer) //devo mandare i dati user anche al client?
+			//encoder:= json.NewEncoder(conn)
+			//encoder.Encode(utente)
+
 		case "ADDGAME ":
-			name:=bufio.NewReader(conn).ReadString('\n')
+			name,_:=bufio.NewReader(conn).ReadString('\n')
+			name = strings.ReplaceAll(name, "\n", "")
 			//ricerca gioco tramite igdb API
 			igdbClient:= igdb.NewClient(id, token, nil)
-			gameList,err:=igdbClient.Search(name, igdb.SetLimit(30), igdb.SetFields("name"))
+			gameList,err:= igdbClient.Search(name, igdb.SetLimit(30), igdb.SetFields("name"))
 			for _,v:= range gameList {
 				buffer = []byte(v.Name)
 				conn.Write(buffer)
 			}
 			conn.Write([]byte("*")) //indica la fine della lista giochi
 			gameName,_:=bufio.NewReader(conn).ReadString('\n')
-			//aggiungere in gameList di user
-			utente.setGameList(gameName)
-			done:= addGame(gameName, utente.userID) 
+			gameName = strings.ReplaceAll(gameName, "\n", "")
+			//se salva correttamente nel db lo aggiunge in gameList di utente
+			done:= addGame(gameName, utente.UserID) 
 			if done {
+				utente.setGameList(gameName)
 				buffer=[]byte("1")
 			} else {
 				buffer=[]byte("0")
 			}
+			conn.Write(buffer)
+
 		case "ADDFRIEND ":
 			gameName,_:=bufio.NewReader(conn).ReadString('\n')
+			gameName = strings.ReplaceAll(gameName, "\n", "")
 			userMap:= findUser(gameName)
 			//decidere se fare mutua amicizia con richiesta o solo il segui
-			delete(userMap, utente.userID)			
+			delete(userMap, utente.UserID)			
 			//invia lista utenti
 			for _,uName:= range userMap {
 				buffer = []byte(uName)
 				conn.Write(buffer)
 			}
 			conn.Write([]byte("*"))
+			
 			//riceve nick friend
 			friend,_:= bufio.NewReader(conn).ReadString('\n')
+			friend = strings.ReplaceAll(friend, "\n", "")
+			if friend=="ABORT" {
+				break
+			}
 			var done bool
 			for uID:= range userMap {
 				if userMap[uID]==friend {
-					done = addFriend(utente.userID, uID)
+					done = followUser(utente.UserID, uID)
 					return
 				}
 			}
 			if done {
+				utente.setFollowingList(friend)
 				buffer=[]byte("1")
 			} else {
 				buffer=[]byte("0")
 			}
+			conn.Write(buffer)
+
 		default:
 			fmt.Println(request)
 		}
