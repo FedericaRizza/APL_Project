3d1aa72230900dcda17827b636cf701b761625ba
Federica Rizza <uni303002@studium.unict.it>
2023-03-10 22:06:29 +0100

    Register form modified

--- client/Form1.cs
+++ client/Form1.cs
@@ -17,13 +17,39 @@ namespace client
 
         private void buttonRegister_Click(object sender, EventArgs e)
         {
+            if (textBoxNick.Text.Length == 0)
+            {
+                MessageBox.Show("Inserire nickname");
+                return;
+            }
+
+            if (textBoxPsw.Text.Length == 0)
+            {
+                MessageBox.Show("Inserire password");
+                return;
+            }
+
+            if (textBoxPsw2.Text.Length == 0)
+            {
+                MessageBox.Show("Ripetere password");
+                return;
+            }
+
             if (textBoxPsw.Text.Equals(textBoxPsw2.Text))
             {
                 if (Client.SendRegister(textBoxNick.Text, textBoxPsw.Text))
+                { 
                     MessageBox.Show("Account creato correttamente.");
+                    this.Hide();
+                    LogForm log = new LogForm(this);
+                    log.Show();                    
+                }
+                    
+                    
+                   
                 else
                     MessageBox.Show("Nickname già esistente");
-
+                
             }
             else
             {
