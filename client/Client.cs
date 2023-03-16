ba393739367f1004c2887f5c24ab081f1ec0cbab
Federica Rizza <uni303002@studium.unict.it>
2023-03-16 04:47:12 +0100

    Bugfix, added Logout and Exit functions

--- client/Client.cs
+++ client/Client.cs
@@ -8,6 +8,7 @@ using System.Net.Sockets;
 using System.Net;
 using System.Security.Cryptography;
 using System.Security.Policy;
+using System.Runtime.CompilerServices;
 
 namespace client
 {
@@ -15,20 +16,17 @@ namespace client
     {
         private static Socket client;
         public static User utente;
+        
         public static void StartConnection()
-        {
-            //byte[] buffer = new byte[1024];
-            
+        {            
             IPHostEntry host = Dns.GetHostEntry("localhost");
             IPAddress ipAddress = host.AddressList[0];
             IPEndPoint localEndPoint = new (ipAddress, 8000);
-            client = new (ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            client.Connect(localEndPoint); //gestire eccezione
-
-            //byte[] msg = Encoding.ASCII.GetBytes("stringa di prova\n");
-
-            //conn.Send(msg);
-            //conn.Close();
+            Socket c = new (ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            c.Connect(localEndPoint); //gestire eccezione
+            client = c;
+            //inizializzo le liste altrimenti non posso aggiungere i dati se sono null
+            
         }
 
         public static bool SendRegister(String nick, String psw) {
@@ -36,10 +34,15 @@ namespace client
             String hash;
             using (SHA256 mySHA=SHA256.Create())
             {
-                buffer= mySHA.ComputeHash(Encoding.ASCII.GetBytes(psw));
-                hash = Encoding.ASCII.GetString(buffer);
+                byte[] data = mySHA.ComputeHash(Encoding.UTF8.GetBytes(psw));
+                var builder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    builder.Append(data[i].ToString("x2"));
+                }
+                hash = builder.ToString();
             }
-            byte[] msg = Encoding.ASCII.GetBytes("REGISTER "+nick+" "+hash+"\n");
+            byte[] msg = Encoding.ASCII.GetBytes("REGISTER\n"+nick+" "+hash+"\n");
             client.Send(msg);
             //Array.Clear(msg);
             Array.Clear(buffer);
@@ -48,7 +51,8 @@ namespace client
             if (len == 0)
                 return false;
 
-            if (Encoding.ASCII.GetString(buffer).Equals("1"))             
+            byte[] reply = buffer.Take(len).ToArray();
+            if (Encoding.ASCII.GetString(reply).Equals("1"))           
                 return true;     
             
             else return false;
@@ -60,25 +64,34 @@ namespace client
             String hash;
             using (SHA256 mySHA = SHA256.Create())
             {
-                buffer = mySHA.ComputeHash(Encoding.ASCII.GetBytes(psw));
-                hash = Encoding.ASCII.GetString(buffer);
+                //algoritmo per portare hash da array di byte a string, dal sito microsoft
+                byte[] data = mySHA.ComputeHash(Encoding.UTF8.GetBytes(psw));
+                var builder = new StringBuilder();
+                for (int i=0; i<data.Length; i++){
+                    builder.Append(data[i].ToString("x2"));
+                }
+                hash = builder.ToString();
             }
-            byte[] msg = Encoding.ASCII.GetBytes("LOGIN " + nick + " " + hash + "\n");
+            byte[] msg = Encoding.ASCII.GetBytes("LOGIN\n" + nick + " " + hash + "\n");
             client.Send(msg);
             Array.Clear(buffer);
             int len = client.Receive(buffer);
 
             if (len == 0) 
                 return false;
-
-            if (Encoding.ASCII.GetString(buffer).Equals("1"))
+            byte[] reply = buffer.Take(len).ToArray();
+            if (Encoding.ASCII.GetString(reply).Equals("1"))
             { 
                 //salvataggio dati utente in locale
                 Array.Clear(buffer);
                 
-                client.Receive(buffer);
+                len= client.Receive(buffer);
                 byte[] jsonObj = buffer.Take(len).ToArray();
                 utente = JsonSerializer.Deserialize<User>(jsonObj);
+                if (utente.GameList == null)
+                    utente.GameList = new List<String>();
+                if (utente.FollowingList == null)
+                    utente.FollowingList = new Dictionary<int, String>();
                 return true;
             }
                 
@@ -88,29 +101,40 @@ namespace client
 
         public static string[] SearchUser(String GameName)
         {
-            String[] userList = null;
+            IList<String> userList = new List<String>();
             byte[] buffer = new byte[1024];
-            byte[] msg = Encoding.ASCII.GetBytes("FOLLOW " + GameName + "\n");
+            byte[] msg = Encoding.ASCII.GetBytes("FOLLOW\n" + GameName + "\n");
             client.Send(msg);
             int len = client.Receive(buffer);
 
             if (len == 0)
-                return userList;
-            while (!Encoding.ASCII.GetString(buffer).Equals("*"))
             {
-                userList.Append(Encoding.ASCII.GetString(buffer));
-                Array.Clear(buffer);
-                client.Receive(buffer);
+                Array.Clear(msg);
+                msg = Encoding.ASCII.GetBytes("ABORT\n");
+                client.Send(msg);
+                return userList.ToArray<String>();
             }
 
-            return userList;
+            byte[] reply = buffer.Take(len).ToArray();
+            var prova = Encoding.ASCII.GetString(reply);
+            while (!Encoding.ASCII.GetString(reply).Equals("*"))
+            {
+                client.Send(Encoding.ASCII.GetBytes("-"));
+                userList.Add(Encoding.ASCII.GetString(reply));
+                Array.Clear(buffer);                
+                len = client.Receive(buffer);
+                reply = buffer.Take(len).ToArray();
+            }
+            if (!userList.Any())
+                client.Send(Encoding.ASCII.GetBytes("ABORT\n"));
+            return userList.ToArray<String>();
 
         }
 
-        public static bool FollowUser(String Name)
+        public static bool FollowUser(String UserName)
         {
             byte[] buffer = new byte[1024];
-            byte[] msg = Encoding.ASCII.GetBytes(Name + "\n");
+            byte[] msg = Encoding.ASCII.GetBytes(UserName + "\n");
             client.Send(msg);
 
             int len = client.Receive(buffer);
@@ -118,10 +142,19 @@ namespace client
             if (len == 0)
                 return false;
 
-            if (Encoding.ASCII.GetString(buffer).Equals("1"))
+            byte[] reply = buffer.Take(len).ToArray();
+            if (!Encoding.ASCII.GetString(reply).Equals("error"))
             {
-                utente.FollowingList.Add(Name);
-                return true;
+                var id = Encoding.ASCII.GetString(reply);
+                int idUser;
+                if (int.TryParse(id, out idUser))
+                {
+                    utente.FollowingList.Add(idUser, UserName);
+                    return true;
+                }
+                else
+                    return false;
+                
             }
 
             else return false;
@@ -135,22 +168,32 @@ namespace client
 
         public static String[] SearchGame(String GameName)
         {
-            String[] gameList = null;
+            IList<String> gameList= new List<String>();
             byte[] buffer = new byte[1024];
-            byte[] msg = Encoding.ASCII.GetBytes("ADDGAME " + GameName);
+            byte[] msg = Encoding.ASCII.GetBytes("ADDGAME\n" + GameName + "\n");
             client.Send(msg);
             int len = client.Receive(buffer);
 
             if (len == 0)
-                return gameList;
-            while (!Encoding.ASCII.GetString(buffer).Equals("*"))
             {
-                gameList.Append(Encoding.ASCII.GetString(buffer));
-                Array.Clear(buffer);
-                client.Receive(buffer);
+                Array.Clear(msg);
+                msg = Encoding.ASCII.GetBytes("ABORT\n");
+                client.Send(msg);
+                return gameList.ToArray<String>();
+
             }
 
-            return gameList;
+            byte[] reply = buffer.Take(len).ToArray();
+            while (!Encoding.ASCII.GetString(reply).Equals("*"))
+            {
+                client.Send(Encoding.ASCII.GetBytes("-"));
+                gameList.Add(Encoding.ASCII.GetString(reply));
+                Array.Clear(buffer);
+                len = client.Receive(buffer);
+                reply = buffer.Take(len).ToArray();
+            }
+
+            return gameList.ToArray<String>();
         }
 
         public static bool AddGame(String GameName)
@@ -160,15 +203,31 @@ namespace client
             client.Send(msg);
 
             int len = client.Receive(buffer);
+
             if (len == 0) 
                 return false;
-            if (Encoding.ASCII.GetString(buffer).Equals("1"))
+
+            byte[] reply = buffer.Take(len).ToArray();
+            if (Encoding.ASCII.GetString(reply).Equals("1"))
             {
                 utente.GameList.Add(GameName);
                 return true;
             }
             else
                 return false;
+        }
+
+        public static void Logout()
+        {
+            utente = new User();
+        }
+
+        public static void Exit()
+        {
+            //comunicare al client che si sta chiudendo la connessione
+            byte[] msg = Encoding.ASCII.GetBytes("CLOSE\n");
+            client.Send(msg);
+            client.Close();
         }
 
     }
