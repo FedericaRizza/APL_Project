4809fe68029650156f1a74270c9a48cd9af178fe
Federica Rizza <uni303002@studium.unict.it>
2023-03-18 04:42:45 +0100

    Added ChatList and SharedGames fields

--- server/user.go
+++ server/user.go
@@ -6,10 +6,13 @@ type user struct {
 	Nick string `json:"Nick"`
 	GameList []string `json:"GameList"`
 	FollowingList map[int]string `json:"FollowingList"`
+	ChatList []string `json:"ChatList"`
+	SharedGames map[string][]string `json:"SharedGames"`
 }
 
 func newUser() user {
-	u := user{GameList : make([]string,0), FollowingList : make(map[int]string)}
+	m:= make(map[string][]string)
+	u := user{GameList : make([]string,0,5), FollowingList : make(map[int]string), ChatList : make([]string,0,5), SharedGames : m}
 	return u
 }
 
@@ -19,6 +22,14 @@ func (u *user) setGameList(game string) {
 
 func (u *user) setFollowingList(fID int, name string) {
 	u.FollowingList[fID] = name
+}
+
+func (u *user) setChatList(name string) {
+	u.ChatList = append(u.ChatList, name)
+}
+
+func (u *user) setSharedGames (game string, name string) {
+	u.SharedGames[game] = append(u.SharedGames[game], name)
 }
 
 /*
