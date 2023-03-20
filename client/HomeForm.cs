cfeae483d3ba3dc14926f98c5acd89333d6bee51
Federica Rizza <uni303002@studium.unict.it>
2023-03-20 01:12:53 +0100

    Added thread listener and delegate for chat update

--- client/HomeForm.cs
+++ client/HomeForm.cs
@@ -10,18 +10,46 @@ using System.Windows.Forms;
 
 namespace client
 {
+    //visibilità namespace, così tutte le classi possono creare delegati ChatDel
+    public delegate void ChatDel(MsgData newMsg);
     public partial class HomeForm : Form
     {
+        private ChatDel del;
         private LogForm login;
+        //string e non list perchè può essere aperta solo una chat per volta?
+        private ChatForm chatOpened;
         public HomeForm(LogForm log)
         {
             InitializeComponent();
             login = log;
             labelUser.Text = "Ciao, " + Client.utente.Nick;
             listBoxGames.Items.AddRange(Client.utente.GameList.ToArray<String>());
-            listBoxFollowing.Items.AddRange(Client.utente.FollowingList.Values.ToArray());
-        }
+            //listBoxFollowing.Items.AddRange(Client.utente.FollowingList.Values.ToArray());
+            listBoxChat.Items.AddRange(Client.utente.ChatList.ToArray<String>());
+            //fare dei metodi anzichè chiamare le proprietà?
 
+            ChatDel delHome = UpdateChat;
+            del = delHome;
+            Thread listener = new Thread(() => Client.ChatListener(del));
+
+        }
+        public void UpdateChat(MsgData newMsg)
+        {
+            if (!chatOpened.ReceiverName.Equals(newMsg.Receiver))
+            {
+                ChatForm chat = new ChatForm(newMsg.Receiver);
+                //var conv = Client.OpenChat(newMsg.Receiver);
+                chat.Show();
+                chatOpened = chat;
+            }
+            else
+            {
+                chatOpened.UpdateChat(newMsg);
+            }
+
+
+        }
+        
         private void labelUser_MouseHover(object sender, EventArgs e)
         {
             panelUser.BringToFront();
@@ -64,6 +92,41 @@ namespace client
             Client.Logout();
             this.Close(); //fare exit o tornare al login?
             login.Show();
+        }
+
+        private void listBoxFollowing_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listBoxFollowing.SelectedItem!= null)
+            {
+                var user = listBoxFollowing.SelectedItem.ToString();
+                listBoxFollowing.ClearSelected();
+                //controlla se la chat è già aperta, aggiungere listener close
+                if (!chatOpened.ReceiverName.Equals(user))
+                {
+                    ChatForm chat = new ChatForm(user);
+                    chatOpened = chat;
+                    chat.Show();
+                    chat.FormClosing += Chat_FormClosing;
+                    listBoxChat.Refresh();
+                }
+            }
+           
+        }
+
+        private void Chat_FormClosing(object? sender, FormClosingEventArgs e)
+        {
+            chatOpened=null;
+            
+        }
+
+        private void listBoxGames_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (listBoxGames.SelectedItem != null)
+            {
+                var game = listBoxGames.SelectedItem.ToString();
+                listBoxFollowing.Items.Clear();
+                listBoxFollowing.Items.AddRange(Client.utente.SharedGames[game]);
+            }
         }
     }
 }
