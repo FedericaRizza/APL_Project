102536f5a6030fd18fbd85399291d87b75a56a16
Federica Rizza <uni303002@studium.unict.it>
2023-03-21 16:02:12 +0100

    Code cleaning

--- client/Program.cs
+++ client/Program.cs
@@ -7,12 +7,10 @@ namespace client
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
-        private static Socket client;
+        
         [STAThread]
         static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+        {            
             ApplicationConfiguration.Initialize();
             Client.StartConnection();
             Application.Run(new LogForm());
